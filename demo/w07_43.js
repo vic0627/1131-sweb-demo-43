358be7f4af5866da56b8ae83ba1c08ad126faa9a
victor_xu <pure90719@gmail.com>
2024-10-26 12:44:23 +0800

    W07-P4: Create navbar with links to class demo w02, w03, w06, and deploy it to Vercel

--- demo/w07_43.js
+++ demo/w07_43.js
@@ -1,1 +1,24 @@
-alert("testing");
+const $navCenter = document.querySelector("#nav-center");
+const $showClassDemo = document.querySelector("#show-class-demo");
+
+const showDemo = (type) => {
+  const iframe = document.createElement("iframe");
+  iframe.width = "100%";
+  iframe.height = "100%";
+  iframe.src = `./${type}/index.html`;
+  $showClassDemo.innerHTML = iframe.outerHTML;
+};
+
+const createNavListItem = (type) => {
+  const li = document.createElement("li");
+  li.addEventListener("click", () => showDemo(type));
+  const a = document.createElement("a");
+  a.setAttribute("href", "javascript:;");
+  a.innerText = type;
+  li.append(a);
+  return li;
+};
+
+["w02_intro_43", "w03_card_43", "w06_blog_43"].forEach((type) => {
+  $navCenter.append(createNavListItem(type));
+});
