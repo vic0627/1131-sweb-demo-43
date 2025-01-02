a4e41eb398070d7ec703478e04cf0ca311c683fa
VICTOR_HSU <pure90719@gmail.com>
2025-01-02 21:02:36 +0800

    update: add nav logic

--- js/classdemo_43.js
+++ js/classdemo_43.js
@@ -5,5 +5,9 @@ const $iframe = $('#show-midterm-answer__frame')
 
 function showClassDemo(page) {
   if (page === 'w07_nav_43') return
+  if (page === 'final_43') {
+    $iframe?.setAttribute('src', 'exams/final1_43/final_p1_43.html')
+    return
+  }
   $iframe?.setAttribute('src', `./demo/${page}/index.html`)
 }
